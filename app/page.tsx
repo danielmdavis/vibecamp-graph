4333e0281045f2e7e1362ae6e7ae44d257962028
danielmdavis <danis1911@gmail.com>
2023-04-21 15:57:11 -0400

    chart data generations changing onclick

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react'
+import React, { useRef } from 'react'
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -9,8 +9,7 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js'
-import { Bar } from 'react-chartjs-2'
-import { faker } from '@faker-js/faker'
+import { Bar, getElementsAtEvent } from 'react-chartjs-2'
 
 ChartJS.register(
   CategoryScale,
@@ -60,9 +59,48 @@ export const data = {
 
 export default function Home() {
 
+  const data1 = {
+    labels,
+    datasets: [
+      {
+        label: 'yesterday',
+        data: labels.map((nomen: string) => dataset1.filter((item: any) => { return item.name === nomen})[0].oldScore),
+        backgroundColor: 'rgba(255, 99, 132, 0.5)'
+      }
+    ]
+  }
+
+  const data2 = {
+    labels,
+    datasets: [
+      {
+        label: 'today',
+        data: labels.map((nomen: string) => dataset1.filter((item: any) => { return item.name === nomen})[0].score),
+        backgroundColor: 'rgba(53, 162, 235, 0.5)'
+      }
+    ]
+  }
+
+  let dataOption = data1
+
+  const chartRef = useRef<ChartJS>(null)
+  const onClick = (event: any) => {
+    const chart: any = chartRef.current
+    console.log(getElementsAtEvent(chart, event))
+
+    if (dataOption === data1) {
+      dataOption = data2
+    } else {
+      dataOption = data1
+    }
+    chart.data = dataOption
+    chart.update('active')
+
+  }
+
   return (
     <main>
-      <Bar options={options} data={data} />
+      <Bar ref={chartRef} options={options} data={dataOption} onClick={onClick} />
     </main>
   )
 }
