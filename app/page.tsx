1a322427adbe28b2241664b3eb30a59caac486f5
danielmdavis <danis1911@gmail.com>
2023-05-09 18:40:46 -0400

    request

--- app/page.tsx
+++ app/page.tsx
@@ -57,9 +57,6 @@ const options: any = {
   maintainAspectRatio: false,
   events: [], // hides tooltip
   indexAxis: 'y',
-  shadowBlur: 3,
-  shadowOffsetX: 3,
-  shadowOffsetY: 10,
   animations: {
     tension: {
       duration: 5500,
@@ -152,34 +149,36 @@ export default function Home() {
 // }
 
 
-  // const getUpdates = async () => {
 
-  //   const myHeaders = new Headers()
-  //   // myHeaders.append('Authorization', 'Basic YWxpY2Vtb3R0b2xhQGdtYWlsLmNvbTpuZXJ2b3VzNm5lbGx5NA==')
-  //   myHeaders.append('Authorization', 'Basic alicemottola@gmail.com:nervous6nelly4')
-  //   // myHeaders.append("Cookie", '_gt_persistent_session=6cddfb372bde63acc544298746360656')
+  // look at curl library - for request as subprocess
+  const getUpdates = async () => {
 
-  //   const requestOptions = {
-  //     method: 'GET',
-  //     mode: 'cors' as RequestMode,
-  //     redirect: 'follow' as RequestRedirect,
-  //     headers: myHeaders
-  //   }
+    const myHeaders = new Headers()
+    // myHeaders.append('Authorization', 'Basic YWxpY2Vtb3R0b2xhQGdtYWlsLmNvbTpuZXJ2b3VzNm5lbGx5NA==')
+    // myHeaders.append("Cookie", '_gt_persistent_session=6cddfb372bde63acc544298746360656')
+    myHeaders.append('Authorization', 'Basic alicemottola@gmail.com:nervous6nelly4')
 
-  //   fetch('https://www.guidedtrack.com/programs/22568/csv', requestOptions)
-  //     // .then(response => response.text())
-  //     .then((response) => { return response })
-  //     .then(response => console.log(response))
-  //     .then(request => console.log(request))
-  //     .then(result => console.log(result))
-  //     // .then(response => setRaw(response))
-  //     .catch(error => console.log('error', error))
+    const requestOptions = {
+      method: 'GET',
+      mode: 'no-cors' as RequestMode,
+      redirect: 'follow' as RequestRedirect,
+      headers: myHeaders
+    }
 
-  // }
+    fetch('https://www.guidedtrack.com/programs/22568/csv', requestOptions)
+      // .then(response => response.text())
+      .then((response) => { return response })
+      .then(response => console.log(response))
+      .then(request => console.log(request))
+      .then(result => console.log(result))
+      // .then(response => setRaw(response))
+      .catch(error => console.log('error', error))
 
-  // useEffect(() => {
-  //   getUpdates()
-  // }, [])
+  }
+
+  useEffect(() => {
+    getUpdates()
+  }, [])
 
 
   const getAllUserData = async () => {
@@ -222,6 +221,10 @@ export default function Home() {
         data: currentArr,
         pointStyle: 'rectRounded',
         backgroundColor: colors.pink,
+        backgroundShadowColor: colors.black,
+        shadowBlur: 3,
+        shadowOffsetX: 3,
+        shadowOffsetY: 10,
         borderWidth: 1.25,
         borderColor: colors.blue2
       }
