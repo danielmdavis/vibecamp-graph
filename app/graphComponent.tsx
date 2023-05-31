02109091726b4d3dfc83d9d77bd534b0ad1f9efe
danielmdavis <danis1911@gmail.com>
2023-05-31 12:33:33 -0400

    fixed pip jutter

--- app/graphComponent.tsx
+++ app/graphComponent.tsx
@@ -125,7 +125,93 @@ export default function Graph(props: { data: any }) {
   //
   // //
 
-  const options: any = chartConfig
+ const options: any = chartConfig
+
+ let pipCounter = -1
+
+//  const options = { 
+//    responsive: true,
+//    maintainAspectRatio: true,
+//    events: '',
+//    indexAxis: 'y',
+//    animations: {
+//      tension: {
+//        duration: 5500,
+//        easing: 'easeOutBounce'
+//      }
+//    },
+//    scales: {
+//      y: 
+//          {
+//            id: 'names',
+//            stacked: true,
+//            position: {
+//              y: 1.5
+//            },
+//            ticks: {
+//              // display: false,
+//              beginAtZero: true,
+//              font: {
+//                family: 'Tan Buster',
+//                size: 30
+//              },
+//              // maxRotation: 22.5,
+//              // minRotation: 22.5,
+//              color: 'rgb(188,239,246)',
+//              mirror: true,
+//              z: 2
+//            },
+//            grid: {
+//              display: false,
+//              color: 'rgba(75,182,203,0)'
+//            },
+//          },
+//      x: {
+//        stacked: true,
+//        // position: {
+//        //   y: 20
+//        // },
+//        ticks: {
+//          display: false,
+//          color: 'rgb(230,227,120)',
+//        },
+//        grid: {
+//          color: 'rgba(75,182,203,0)'
+//        }
+//      }
+//    },
+//    plugins: {
+//      legend: {
+//        display: false,
+//        position: 'bottom',
+//        labels: {
+//          color: 'rgb(230,227,120)',
+//          font: {
+//            family: 'Space Grotesk'
+//          }, 
+//        }
+//      },
+//      title: {
+//        display: true,
+//        text: 'Click to see the full history of romantic acclaim',
+//        color: 'rgb(242,215,170)',
+//        font: {
+//          family: 'Space Grotesk',
+//          size: 18
+//        }, 
+//        position: 'bottom'
+//      },
+//      datalabels: {
+//        formatter: (item: any) => {
+//          pipCounter += 1
+//          console.log(pipCounter)
+//          return currentArr[pipCounter - currentArr.length]
+//        }
+//      }
+//    }
+//  }  
+
+
 
   // db get
   useEffect(() => {
@@ -203,7 +289,11 @@ export default function Graph(props: { data: any }) {
   if (chart !== null && newX !== -Infinity) {
     chart.config.options.scales.x.max = newX
   }
+
   setDate(chart)
+  if (chart !== null && chart.data.datasets[1]) {
+    chart.options.animation.duration = 0
+  }
 
   const onClick = (event: any) => {
     const chart: any = chartRef.current
